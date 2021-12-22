fc242d84e147c1936adf7935656755e9a3458439
haesoo <wjdgotn77@gmail.com>
2021-12-22 20:37:58 +0900

    [Refactor: 피어리뷰 후 수정사항 반영 / map 사용]

--- src/pages/haesoo/Main/MainLeft/StoryFeed/StoryFeed.js
+++ src/pages/haesoo/Main/MainLeft/StoryFeed/StoryFeed.js
@@ -1,51 +1,17 @@
 import React from 'react';
 import './StoryFeed.scss';
 
-export default function StoryFeed() {
-  return (
-    <div className="story-container-haesoo">
-      <div className="story-container__contents">
-        <div className="story__image">
-          <img alt="story profile" src="/images/haesoo/story_images/1.JPG" />
-        </div>
-        <div className="story__id">onnuk_</div>
+export default function StoryFeed(props) {
+  const { storyDataList } = props;
+
+  const stories = storyDataList.map(story => (
+    <div className="story-container__contents">
+      <div className="story__image">
+        <img alt={story.alt} src={story.src} />
       </div>
-      <div className="story-container__contents">
-        <div className="story__image">
-          <img alt="story profile" src="/images/haesoo/story_images/2.JPG" />
-        </div>
-        <div className="story__id">singmulstore</div>
-      </div>
-      <div className="story-container__contents">
-        <div className="story__image">
-          <img alt="story profile" src="/images/haesoo/story_images/3.JPG" />
-        </div>
-        <div className="story__id">dlwldms</div>
-      </div>
-      <div className="story-container__contents">
-        <div className="story__image">
-          <img alt="story profile" src="/images/haesoo/story_images/7.JPG" />
-        </div>
-        <div className="story__id">_jeongjaehyun</div>
-      </div>
-      <div className="story-container__contents">
-        <div className="story__image">
-          <img alt="story profile" src="/images/haesoo/story_images/5.JPG" />
-        </div>
-        <div className="story__id">cy_1.018</div>
-      </div>
-      <div className="story-container__contents">
-        <div className="story__image">
-          <img alt="story profile" src="/images/haesoo/story_images/4.JPG" />
-        </div>
-        <div className="story__id">bona_is_cute</div>
-      </div>
-      <div className="story-container__contents">
-        <div className="story__image">
-          <img alt="story profile" src="/images/haesoo/story_images/6.JPG" />
-        </div>
-        <div className="story__id">ninicho</div>
-      </div>
+      <div className="story__id">{story.id}</div>
     </div>
-  );
+  ));
+
+  return <div className="story-container-haesoo">{stories}</div>;
 }
