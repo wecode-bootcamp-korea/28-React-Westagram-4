e4072be9fc63ce1718c4ef8f18d77aeb4bde7499
haesoo <wjdgotn77@gmail.com>
2021-12-24 16:32:49 +0900

    - Login.js
     id와 password 의 state를 나눠 관리했던 것을 하나의 state에서 관리하는 것으로 수정하였습니다.
     id의 유효성 검사 로직을 더 직관적인 코드로 수정하였습니다.

--- src/pages/haesoo/Login/Login.js
+++ src/pages/haesoo/Login/Login.js
@@ -3,101 +3,90 @@ import { useNavigate } from 'react-router-dom';
 import Footer from '../../../components/Footer/Footer';
 import './Login.scss';
 
+const BASE_URL = 'http://10.58.6.234:8000/users/login';
+
 function Login() {
-  const [isIdValid, setIsIdValid] = useState('');
-  const [isPwValid, setIsPwValid] = useState('');
+  const [input, setInput] = useState({
+    id: '',
+    password: '',
+  });
+
+  const inputValue = event => {
+    const { name, value } = event.target;
+    setInput({ ...input, [name]: value });
+  };
+
+  const isFormValid = input.id.includes('@') && input.password.length > 6;
 
   const navigate = useNavigate();
   const goToMain = () => {
     navigate('/main-haesoo');
   };
 
-  const handleIdChange = event => {
-    const { value } = event.target;
-    setIsIdValid(value);
-  };
-
-  const handlePwChange = event => {
-    const { value } = event.target;
-    setIsPwValid(value);
-  };
-
-  const checkedValid = () => {
-    return isIdValid.includes('@') && isPwValid.length > 6 ? false : true;
-  };
-
   const handleEnter = event => {
-    if (checkedValid && event.key === 'Enter') return fetchData();
+    if (event.key === 'Enter') return fetchData();
   };
 
   const fetchData = () => {
-    fetch('http://10.58.3.173:8000/users/signup', {
-      method: 'POST',
-      body: JSON.stringify({
-        email: isIdValid,
-        password: isPwValid,
-        userName: '정해수',
-        mobile: '010-7226-2627',
-      }),
-    })
-      .then(response => response.json())
-      .then(result => {
-        if (result.message === 'INVALID PASSWORD') {
-          alert(
-            '비밀번호는 8자리 이상, 문자,숫자,특수 기호 모두 한가지 이상 입력해주세요.'
-          );
-        } else if (result.message === 'EXIST EMAIL') {
-          alert('존재하는 이메일 입니다.');
-        } else if (result.message === 'INVALID EMAIL') {
-          alert('이메일을 다시 입력해주세요.');
-        } else if (result.message === 'SUCCESS') {
-          alert('회원가입을 축하합니다.');
-          goToMain();
-        }
-      });
+    // fetch(BASE_URL, {
+    //   method: 'POST',
+    //   body: JSON.stringify({
+    //     email: id,
+    //     password: password,
+    //     username: '정해수',
+    //     phone_number: '010-7226-2627',
+    //     age: 28,
+    //   }),
+    // })
+    //   .then(response => response.json())
+    //   .then(result => {
+    //     localStorage.setItem('token', result.access_token);
+    //   });
+
+    goToMain();
   };
 
   return (
-    <div className="login">
-      <main className="login-main">
-        <article className="content-container">
-          <section className="login-container">
-            <div className="logo">Westagram</div>
-            <form className="login-form" onKeyPress={handleEnter}>
-              <input
-                onChange={handleIdChange}
-                className="login-form__id js-id"
-                placeholder="  전화번호, 사용자 이름 또는 이메일"
-                type="form"
-              />
-              <input
-                onChange={handlePwChange}
-                className="login-form__password js-password"
-                placeholder="  비밀번호를 입력하세요"
-                type="password"
-              />
-              <button
-                disabled={checkedValid()}
-                onClick={fetchData}
-                className="login-button"
-                type="button"
-              >
-                로그인
-              </button>
-            </form>
-            <p>또는</p>
-            <button className="facebook-button">Facebook으로 로그인</button>
-            <section className="find-password-container">
-              <p>비밀번호를 잊으셨나요?</p>
-            </section>
-          </section>
-          <section className="sign-container">
-            <p>계정이 없으신가요?</p>
-            <span>가입하기</span>
-          </section>
-        </article>
-        <Footer />
-      </main>
+    <div className="login-haesoo">
+      <section className="login-container">
+        <div className="logo">Westagram</div>
+        <form
+          className="login-form"
+          onInput={inputValue}
+          onKeyPress={handleEnter}
+        >
+          <input
+            className="login-form__id"
+            name="id"
+            type="input"
+            placeholder="  전화번호, 사용자 이름 또는 이메일"
+          />
+          <input
+            className="login-form__password"
+            name="password"
+            type="password"
+            placeholder="  비밀번호를 입력하세요"
+          />
+          <button
+            disabled={!isFormValid}
+            className="login-button"
+            type="button"
+            onClick={fetchData}
+          >
+            로그인
+          </button>
+        </form>
+        <p>또는</p>
+        <button className="facebook-button">Facebook으로 로그인</button>
+        <section className="find-password-container">
+          <p>비밀번호를 잊으셨나요?</p>
+        </section>
+      </section>
+      <section className="sign-container">
+        <p>계정이 없으신가요?</p>
+        <span>가입하기</span>
+      </section>
+      <Footer />
     </div>
   );
 }
